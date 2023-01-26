3f8ed000139907469b6ecee7f34c85386441c04d
ondrej <pesej.ondrek@gmail.com>
2023-01-26 12:55:27 +0100

    gscript -> pygrass

--- applications/distances.py
+++ applications/distances.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import grass.script as gs
-
-
 from grass.pygrass.modules import Module
 
 
--- applications/drainage_path.py
+++ applications/drainage_path.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
-import grass.script as gs
+from grass.pygrass.modules import Module
 
 
 def run_drainage_path(real_elev, scanned_elev, env, **kwargs):
 
-    gs.run_command('g.region', flags='p',  raster=scanned_elev)
+    Module('g.region', flags='p',  raster=scanned_elev)
 
 
-    gs.run_command('r.watershed', elevation=scanned_elev, 
+    Module('r.watershed', elevation=scanned_elev, 
 
                     accumulation='accum', drainage='drain_dir', overwrite=True)
 
-    gs.run_command('r.mapcalc', 
+    Module('r.mapcalc', 
 
                     expression="drain_deg = if(drain_dir != 0, 45. * abs(drain_dir), null())", 
 
                     overwrite=True)
 
-    gs.run_command('r.path', input='drain_deg', raster_path='drain_path',
+    Module('r.path', input='drain_deg', raster_path='drain_path',
 
                     vector_path='drain_path', start_coordinates=(635184 ,226127 ), overwrite=True)
 
-    gs.run_command('r.colors', map=scanned_elev, color='elevation', overwrite=True)
+    Module('r.colors', map=scanned_elev, color='elevation', overwrite=True)
 
-    gs.run_command('r.relief', input=scanned_elev, output='relief', overwrite=True)
+    Module('r.relief', input=scanned_elev, output='relief', overwrite=True)
 
