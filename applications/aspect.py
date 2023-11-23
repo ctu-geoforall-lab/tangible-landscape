3c9e665d8f062859d42b9b55a24c73ccbc8bed4f
Ondrej Pesek <pesej.ondrek@gmail.com>
2023-11-23 17:01:26 +0100

    add aspect

---
+++ applications/aspect.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+##############################################################################
+#
+# AUTHOR(S):    Ondrej Pesek
+# BASED ON:     github.com/tangible-landscape/tangible-landscape-applications/
+##############################################################################
+
+import os
+
+from grass.pygrass.modules import Module
+
+os.environ['GRASS_OVERWRITE'] = '1'
+os.environ['GRASS_VERBOSE'] = '0'
+
+
+def run_aspect(real_elev, scanned_elev, env, **kwargs):
+    Module("g.region", raster=scanned_elev)
+
+    Module("r.slope.aspect", elevation=scanned_elev, aspect="aspect")
+
+    # get optimal interval for contours
+    info = Info(scanned_elev)
+    info.read()
+    interval = (info.max - info.min) / 20
+    # alternatively set fixed interval
+    # interval = 10
+
+    Module(
+        "r.contour",
+        flags="t",
+        input=scanned_elev,
+        output="contours",
+        step=interval,
+    )
